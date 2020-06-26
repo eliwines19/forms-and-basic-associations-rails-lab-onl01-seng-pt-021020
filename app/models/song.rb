e810105038cd4dce3072e965ead4ae9070d00e13
Elijah wines <elijahwines84@yahoo.com>
2020-06-26 13:35:28 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -7,4 +7,8 @@ class Song < ActiveRecord::Base
   def artist_name=(name)
     self.artist = Artist.find_or_create_by(name: name)
   end
+
+  def artist_name
+    self.artist ? self.artist_name : nil
+  end
 end
